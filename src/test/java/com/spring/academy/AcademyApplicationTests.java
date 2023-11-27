ff7b40bbca0fcc2807be3cec966820426e80b38b
Luis Santos <lhsantos89@gmail.com>
2023-11-26 23:01:46 -0300

    testing

--- src/test/java/com/spring/academy/AcademyApplicationTests.java
+++ src/test/java/com/spring/academy/AcademyApplicationTests.java
@@ -1,13 +1,30 @@
 package com.spring.academy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AcademyApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void shouldListToysWithSuccess() throws Exception {
+		MockHttpServletResponse response =
+				mockMvc.perform(MockMvcRequestBuilders.get("/toys"))
+					   .andReturn().getResponse();
+
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
 	}
 
 }
